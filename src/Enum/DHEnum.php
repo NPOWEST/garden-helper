13b196863f94ff96f2c9dd73ac86cad2c9fed100
Virus3D <virus3d@rambler.ru>
2024-07-08 07:28:48 +0700

    add DHEnum

---
+++ src/Enum/DHEnum.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @see https://npowest.ru
+ * @license Shareware
+ * @copyright (c) 2019-2024 NPOWest
+ */
+
+declare(strict_types=1);
+
+namespace Npowest\GardenHelper\Enum;
+
+enum DHEnum: string
+{
+	case d = 'D';
+
+	case h = 'H';
+}//end enum
