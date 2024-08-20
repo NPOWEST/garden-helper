e8a70924624fab7f9908771fb830bc38a41de62b
Virus3D <virus3d@rambler.ru>
2024-08-20 16:14:05 +0700

    update constant

--- src/Constant.php
+++ src/Constant.php
@@ -12,35 +12,41 @@ namespace Npowest\GardenHelper;
 
 final class Constant
 {
-	public const MINUTE = 60;
+	public const DAY = 86_400;
 
-	public const MINUTE_FIVE = 300;
+	public const DAY_MINUTE = 1_440;
+
+	public const HALFDAY = 43_200;
 
 	public const HALFHOUR = 1_800;
 
 	public const HOUR = 3_600;
 
-	public const HALFDAY = 43_200;
+	public const MAX_READ_CNT = 5;
 
-	public const DAY = 86_400;
+	public const MINUTE = 60;
 
-	public const DAY_MINUTE = 1_440;
-
-	public const WEEK = 604_800;
+	public const MINUTE_FIVE = 300;
 
 	public const MONTH = 2_592_000;
 
-	public const YEAR = 31_536_000;
+	public const TM_LAST_READ = 30;
 
-	public const TYPE_NULL = -1;
+	public const TM_LAST_READ_GPRS = 180;
 
-	public const TYPE_HEAT = 0;
+	public const TYPE_3 = 3;
 
 	public const TYPE_CONTR = 1;
 
 	public const TYPE_ELEC = 2;
 
-	public const TYPE_3 = 3;
+	public const TYPE_HEAT = 0;
+
+	public const TYPE_NULL = -1;
 
 	public const TYPE_WATER = 4;
+
+	public const WEEK = 604_800;
+
+	public const YEAR = 31_536_000;
 }//end class
